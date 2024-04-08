7b3ae811f10e86c02dca366d5d6b9f1dbb0e57e0
malveyr <malveyr@msoe.edu>
2024-04-08 10:41:13 -0500

    Fixed Reservations

--- ReservationsScript.js
+++ ReservationsScript.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textarea = document.getElementById('RequestsTextfield');
+    const containerHeight = document.getElementById('ContainerID').offsetHeight - textarea.offsetHeight;
 
     document.addEventListener('mouseup', function () {
-        document.getElementById('ContainerID').style.height = textarea.offsetHeight + 512 + 'px';
+        document.getElementById('ContainerID').style.height = textarea.offsetHeight + containerHeight + 'px';
     });
 });
